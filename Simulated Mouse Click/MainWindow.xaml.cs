276ab0e0dfed439027665ec1ed5228da4a9b64d2
Daniel Mariano <dtgm@uol.com.br>
2013-11-20 18:55:17 -0200

    Test Programs
    1) Sample for Mouse Click Event
    2) Sample for Arduino Data Aquisition

---
+++ Simulated Mouse Click/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Threading;
+
+namespace Simulated_Mouse_Click
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        string RxString;
+        int iCountClicks;
+
+        public MainWindow()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+            InitializeComponent();
+            iCountClicks = 0;
+        }
+
+        private void btTest_Click(object sender, RoutedEventArgs e)
+        {
+            iCountClicks++;
+            lbCount.Content = iCountClicks.ToString();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            DoMouseClick();
+        }
+
+        private void DoMouseClick()
+        {
+            //int X = Cursor.Position.X;
+            //int Y = Cursor.Position.Y;
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+    }
+}
